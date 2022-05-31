631652b5cfdb206b265c3097ee60b039add3164a
leoevgeniy <leoevgeniy@gmail.com>
2022-05-31 14:26:09 +0400

    charlist

---
+++ src/components/errorMessage/errorMessage.js
@@ -0,0 +1,16 @@
+import img from './error.gif'
+
+const errorMessage = () => {
+    return (
+        <img style={{   
+            display: "block", 
+            width: "250px", 
+            height: "250px", 
+            objectFit: "contain", 
+            margin: "0 auto"}} 
+            src={img}
+            alt={'error'} />
+    )
+}
+
+export default errorMessage;
