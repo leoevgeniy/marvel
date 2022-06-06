1b77e9b75350f24a2a1f48dbd0b8124ac215a113
leoevgeniy <leoevgeniy@gmail.com>
2022-06-06 14:58:20 +0400

    chrlist hoocks

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState } from "react";
 import PropTypes from 'prop-types';
 import AppHeader from "../appHeader/AppHeader";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
@@ -8,39 +8,31 @@ import CharInfo from "../charInfo/CharInfo";
 
 import decoration from '../../resources/img/vision.png';
 
-class App extends Component {
-    state= {
-        selectedChar: null
+const App = () => {
+    const [selectedChar, setChar] = useState(null);
+    const onCharSelected = (id) => {
+        setChar(id);
     }
 
-    onCharSelected = (id) => {
-        this.setState({
-            selectedChar: id
-        })
-    }
-
-    render () {
-        return (
-            <div className="app">
-                <AppHeader/>
-                <main>
+    return (
+        <div className="app">
+            <AppHeader/>
+            <main>
+                <ErrorBoundary>
+                    <RandomChar/>
+                </ErrorBoundary>
+                <div className="char__content">
                     <ErrorBoundary>
-                        <RandomChar/>
+                        <CharList onCharSelected={onCharSelected}/>
                     </ErrorBoundary>
-                    <div className="char__content">
-                        <ErrorBoundary>
-                            <CharList onCharSelected={this.onCharSelected}/>
-                        </ErrorBoundary>
-                        <ErrorBoundary>
-                            <CharInfo charID={this.state.selectedChar}/>
-                        </ErrorBoundary>
-                    </div>
-                    <img className="bg-decoration" src={decoration} alt="vision"/>
-                </main>
-            </div>
-        )
-    }
-
+                    <ErrorBoundary>
+                        <CharInfo charID={selectedChar}/>
+                    </ErrorBoundary>
+                </div>
+                <img className="bg-decoration" src={decoration} alt="vision"/>
+            </main>
+        </div>
+    )
 }
 CharInfo.propTypes = {
     onCharSelected: PropTypes.func
--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
 import './style/style.scss';
 
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
