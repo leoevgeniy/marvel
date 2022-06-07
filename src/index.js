eb4e3fba6a88a26cc21716adff75a27bab5be0e1
leoevgeniy <leoevgeniy@gmail.com>
2022-06-07 14:38:28 +0400

    first route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import ReactDOM from 'react-dom/client';
+import React from 'react';
 import App from './components/app/App';
 import './style/style.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    // <React.StrictMode>
+        <App />
+    // </React.StrictMode>
 );
