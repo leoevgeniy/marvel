ecf1e41da5a96dd10c434218b9d6293deb87af08
leoevgeniy <leoevgeniy@gmail.com>
2022-06-12 18:23:15 +0400

    before FSM

--- src/components/pages/SingleComicPage.js
+++ src/components/pages/SingleComicPage.js
@@ -1,4 +1,6 @@
 import { useParams, Link } from 'react-router-dom';
+import Helmet from 'react-helmet';
+
 import './SingleComicPage.scss';
 import { useEffect, useState } from 'react';
 import useMarvelService from '../../services/MarvelService'
@@ -43,6 +45,13 @@ const View = ({comic}) => {
     const {title, description, pageCount, thumbnail, language, price} = comic;
     return(
         <div className="single-comic">
+            <Helmet>
+                <meta
+                 name="description"
+                 content={`${title} comics book`}
+                 />
+                 <title>{title}</title>    
+            </Helmet>
             <img src={thumbnail} alt={title} className="single-comic__img"/>
             <div className="single-comic__info">
                 <h2 className="single-comic__name">{title}</h2>
